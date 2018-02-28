54297fa977f5e2639a59fb3e54699c7c11ee5f1a
eternity6666 <1462928058@qq.com>
2018-02-28 18:15:16 +0800

    the code of demo

---
+++ js/yzhdwy.js
@@ -0,0 +1,36 @@
+var list = document.getElementById("main").getElementsByTagName("div");
+var list2 = new Array;
+
+//使用原生js实现给元素添加id值
+function newArray() {
+    for (var i = 0; i < list.length; i++) {
+        list2[i]=list[i].firstElementChild.innerHTML;
+        list[i].firstElementChild.id=list2[i];
+    }
+};
+
+newArray();
+
+var menu = document.getElementById("menu");
+
+/*
+//这是一个失败的尝试
+for (var i = 0; i < list2.length; i++) {
+    var para = document.createElement("li");
+    var a=document.createElement("a");
+    var node = document.createTextNode('<a href="#' + list2[i] +'">'+list2[i]+'</a>');
+    para.appendChild(node);
+    menu.appendChild(para);
+};
+*/
+//使用原生Javascript实现遍历列表
+for (var i = 0; i < list2.length; i++) {
+    var para = document.createElement("li");
+    var a=document.createElement("a");
+    a.href="#"+list2[i];
+    var node = document.createTextNode(list2[i]);
+    a.appendChild(node);
+    para.appendChild(a);
+    menu.appendChild(para);
+};
+
